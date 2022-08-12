a4475df7cc73719bc8f7decc4b9ab3509801ca16
Muhammad Dio Tsani Iba - 149251970100-113 <dio.tsani123@gmail.com>
2022-08-12 14:27:08 +0800

    init

---
+++ Assets/Scripts/Gameplay/GameplayView.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Agate.MVC.Base;
+using Agate.MVC.Core;
+using ExampleGame.Boot;
+using ExampleGame.Home;
+
+namespace ExampleGame.Gameplay
+{
+    public class GameplayView : BaseSceneView
+    {
+
+    }
+}
+
+
---
+++ Assets/Scripts/Home/HomeView.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using Agate.MVC.Base;
+using Agate.MVC.Core;
+
+
+namespace ExampleGame.Home
+{
+    public class HomeView : BaseSceneView
+    {
+        [SerializeField]
+        private Button _playButton;
+
+        public void SetCallBacks(UnityAction onClickplayButton)
+        {
+            _playButton.onClick.RemoveAllListeners();
+            _playButton.onClick.AddListener(onClickplayButton);
+        }
+    }
+
+}
